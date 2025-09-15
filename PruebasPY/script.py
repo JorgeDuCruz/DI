9544e7f438647f1481188bee36f2ef7e3b9ef157
dam <jdurancruz@danielcastelao.org>
2025-09-15 13:58:34 +0200

    Apuntes python

---
+++ PruebasPY/script.py
@@ -0,0 +1,60 @@
+
+#Numeros
+
+#Enteiros
+numEnteiro = 5
+print(type(numEnteiro))
+
+#Coma flotante
+numDecimal = 5.6
+numDecimal2 = 0.1e-5
+print(type(numDecimal))
+print(type(numDecimal2))
+
+#Booleanos
+booleano = True
+print(type(booleano))
+
+#Complexos
+complexo = 2.3  +9.3j
+print(type(complexo))
+
+#Cadeas
+"""Este é o comentario multiliña
+Onde podemos escribir a biblia si quixera tanto con 3 comillas dobles
+"""
+'''Como con comillas simples
+'''
+cadea = "Esto é unha cadea con comillas \"dobres\" tambien se puede poner comillas simples dentro de comillas dobles('')"
+cadea2 = 'Esto é unha cadea con comillas \'simples\' tambien se puede poner comillas dobles dentro de comillas simples("")'
+print(cadea)
+print(cadea2)
+print(cadea,"\n",cadea2)
+
+
+
+#Operadores aritmeticos
+
+# + -> más
+# - -> menos o valor negaitvo
+# * -> multiplicar
+# / -> dividir
+# ** -> exponente
+# // -> division que se queda en el entero (10/3 = 3.333 pero 10//3 = 3)
+# % -> devuleve el resto de la division (10%3 = 1)
+
+#Operadores a nivel de bit
+# & -> and  se hace un y logico bit a bit (2&3 = 2)
+# | -> or se hace un o logico bit a bit (2|3 = 3)
+# ^ -> xor se hace un xor logico bit a bit (2^3 = 1)
+# ~ -> not. se hace un not logico bit a bit (~2 = -3 -> porque la nomenclatura binaria hace que el primer digito sea el signo y luego funciona raro)
+# >> -> dividir por potencoias de 2 (Realmente muevo los bits a la derecha la cantidad de huecos indicada)
+# << -> multiplicar potencias de 2 (Realmente muevo los bits a la izquierda la cantidad de huecos indicada)
+
+#Operadores de cadea + *
+#+ concadena
+#* replica una cadena el numero de veces indicada
+
+#Java   &&   ||  |
+#Python and  or not
+#Comunes == != < > <= >=
