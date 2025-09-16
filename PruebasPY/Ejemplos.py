277e1d53270004bde5593001cbabd83ab06312e3
dam <jdurancruz@danielcastelao.org>
2025-09-16 14:06:04 +0200

    Prueba de atajos para comit

--- PruebasPY/Ejemplos.py
+++ PruebasPY/Ejemplos.py
@@ -6,6 +6,6 @@ if n1>5:
     print(n1)
 elif(n1==3):
     print("Si es",n1,"nfn")#Para concatenar en lugar de usar "+" se usa simplemente una ",". Cada concatenacion añade autopmaticamente un espacio para que no se peguen las distintas partes
-else:
+else: # Prueba
  print(n1)
  print(n1)
