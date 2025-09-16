1adbe7f9a5fe055fad998b626f10962a1ba47e6c
dam <jdurancruz@danielcastelao.org>
2025-09-16 14:03:26 +0200

    Sentencias logicas

---
+++ PruebasPY/Ejemplos.py
@@ -0,0 +1,11 @@
+#Sentecias condicionales
+import random
+
+n1 = random.randint(1,10)
+if n1>5:
+    print(n1)
+elif(n1==3):
+    print("Si es",n1,"nfn")#Para concatenar en lugar de usar "+" se usa simplemente una ",". Cada concatenacion añade autopmaticamente un espacio para que no se peguen las distintas partes
+else:
+ print(n1)
+ print(n1)
