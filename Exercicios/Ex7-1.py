9b77059b3dc28bc5b3dc14a850683b6e80712242
dam <jdurancruz@danielcastelao.org>
2025-09-22 13:31:27 +0200

    Exercicio 7.1 ahora si funciona que se me olvido la linea de codigo más importante

--- Exercicios/Ex7-1.py
+++ Exercicios/Ex7-1.py
@@ -1,9 +1,10 @@
-t1 = 2,10,58
+t1 = 2,10,9,58
 def isOrderAsc(tupla):
     maximo = tupla[0]
     for num in tupla:
         if maximo>num:
             return False
+        maximo=num
     return True
 
 print(isOrderAsc(t1))
