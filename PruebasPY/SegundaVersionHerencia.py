e2543211c7f1694241b90d12886a1029826974fc
dam <jdurancruz@danielcastelao.org>
2025-09-23 13:55:01 +0200

    comentado commmit anterior

--- PruebasPY/SegundaVersionHerencia.py
+++ PruebasPY/SegundaVersionHerencia.py
@@ -1,18 +1,18 @@
 class Persona:
     """Clase para definir una persona"""
-    def __init__(self,nome,dni,edade,**outros):
+    def __init__(self,nome,dni,edade,**outros): #Outros con ** para recibir mas datos de los que necesita
         self.nome = nome
         self.dni = dni
         self.edade = self.comprobarEdade(edade)
 
     def comprobarEdade(self,edade):
         if (edade>=0) and edade <100:
-            return id
+            return edade
         else:
             return 0
 
 class Posto:
-    def __init__(self,tarea,horario,salario,formacion,**outros):
+    def __init__(self,tarea,horario,salario,formacion,**outros): #Outros con ** para recibir mas datos de los que necesita
         self.tarea = tarea
         self.horario = horario
         self.salario = salario
@@ -20,8 +20,8 @@ class Posto:
 
 class Trabajador (Persona,Posto):
     def __init__(self,nome,dni,edade,tarea,horario,salario,formacion,NUSS):
-        super().__init__(nome=nome,dni=dni,edade=edade,tarea=tarea,horario=horario,salario=salario,formacion=formacion)
+        super().__init__(nome=nome,dni=dni,edade=edade,tarea=tarea,horario=horario,salario=salario,formacion=formacion) # manda a los dos constructores pero con los nombre para que los padres puedan saber que valor recojer
         self.NUSS = NUSS
 
 t2 = Trabajador("Juan",5679,45,"Soldador",7,2300,"CM","13515/UN")
-print(t2)
+print(t2.edade)
