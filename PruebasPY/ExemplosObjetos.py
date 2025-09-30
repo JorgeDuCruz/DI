edf3c196872af17bce384e6d6f2efaebdabc285d
dam <jdurancruz@danielcastelao.org>
2025-09-30 14:47:24 +0200

    Crear excepciones propias

--- PruebasPY/ExemplosObjetos.py
+++ PruebasPY/ExemplosObjetos.py
@@ -1,3 +1,6 @@
+from pyclbr import Class
+
+
 class Persona:
     """Clase para definir una persona"""
     def __init__(self,nome,dni,edade):
@@ -35,3 +38,31 @@ print(p.edade)
 t = Trabajador("Pepe","54L",36,"Hogar","18:30-19",2000,"Niguna",54321)
 
 print(t)
+
+#Excepciones
+
+class ErroIdade (Exception):
+    def __init__(self,edade):
+        self.edade = edade
+    def __str__(self):
+        return "Erro edade inadecuada: " +str(self.edade)+ " incorrecta"
+
+
+class Persona4:
+    """Clase para definir una persona"""
+    def __init__(self,nome,dni,edade):
+        self.nome = nome
+        self.dni = dni
+        self.edade = self.comprobarEdade(edade)
+
+    def comprobarEdade(self,edade):
+        if (edade>=0) and edade <100:
+            return edade
+        else:
+            raise ErroIdade(edade)
+
+try:
+    persoa4 = Persona4("pepe",354,100)
+    juan = Persona4("juan",354,57)
+except ErroIdade:
+    print("Erro de edad ")
