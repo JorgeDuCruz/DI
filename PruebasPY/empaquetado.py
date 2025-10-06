a17de2374a59c0cc8d84fb3d0310738f1f6f7496
dam <jdurancruz@danielcastelao.org>
2025-10-06 12:51:13 +0200

    Importar cosas

---
+++ PruebasPY/empaquetado.py
@@ -0,0 +1,8 @@
+def unha_funcion():
+    print("Unha función")
+
+class UnhaClase:
+    def __init__(self):
+        print("Unha clase")
+
+print("Un módulo")
---
+++ PruebasPY/novoModulo.py
@@ -0,0 +1,7 @@
+import empaquetado # Al importar ejecuta lo que no este dentro de nada, es decir, simula que se ejecuta ese programa.
+
+from math import e # Importa las funcionalidades de la libreria math, con este metodo no hace falta escribir el math antes de la función que quieres usar
+#from math import * # Importa todas las funcionalidades de math
+
+empaquetado.unha_funcion()
+print(e)
