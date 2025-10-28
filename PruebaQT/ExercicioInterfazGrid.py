a104885baa7abd2285b77fa3fcf01174a678bb74
dam <jdurancruz@danielcastelao.org>
2025-10-28 14:19:50 +0100

    Funcionalidades hechas

--- PruebaQT/ExercicioInterfazGrid.py
+++ PruebaQT/ExercicioInterfazGrid.py
@@ -1,4 +1,5 @@
 import sys
+from xxsubtype import bench
 
 from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import (QApplication, QMainWindow, QPushButton, QLabel, QLineEdit, QVBoxLayout, QWidget, QCheckBox,
@@ -8,32 +9,60 @@ import ModeloLista
 
 
 class Interfaz(QMainWindow):
+    def ocultar(self):
+        indices = self.lstVisible.selectedIndexes()
+        if indices:
+            self.modeloOculto.follas.append(self.modeloVisible.follas[indices[0].row()])
+            del self.modeloVisible.follas[indices[0].row()]
+            self.modeloVisible.layoutChanged.emit()
+            self.modeloOculto.layoutChanged.emit()
+            self.lstVisible.clearSelection()
+
+
+    def mostrar(self):
+        indices = self.lstOculta.selectedIndexes()
+        if indices:
+            self.modeloVisible.follas.append( self.modeloOculto.follas[indices[0].row()])
+            del self.modeloOculto.follas[indices[0].row()]
+            self.modeloVisible.layoutChanged.emit()
+            self.modeloOculto.layoutChanged.emit()
+            self.lstOculta.clearSelection()
+
+    def cerrar(self):
+        self.close()
+
     def __init__(self):
         super().__init__()
         listaFollas = ["Folla 1","Folla 2","Folla 3"]
+        self.modeloVisible = ModeloLista.ModeloFollas(listaFollas)
+        self.modeloOculto = ModeloLista.ModeloFollas()
 
         self.setWindowTitle("Primera ventana con QT")
         maia = QGridLayout()
         lblFollasVisibles = QLabel("Follas Visibles")
         lblFollasOcultas = QLabel("Follas Ocultas")
         btnMOstrar = QPushButton("<<Mostrar")
+        btnMOstrar.clicked.connect(self.mostrar)
         btnOcultar = QPushButton("Ocultar>>")
+        btnOcultar.clicked.connect(self.ocultar)
         btnCerrar = QPushButton("Cerrar")
+        btnCerrar.clicked.connect(self.cerrar)
         btnCerrar.setFixedSize(60,30)
-        lstOculta = QListView()
-        lstVisible = QListView()
-
-        self.modelo = ModeloLista.ModeloFollas(listaFollas)
-        lstVisible.setModel(self.modelo)
+        self.lstOculta = QListView()
+        self.lstVisible = QListView()
 
 
-        controlsInerte = QPushButton()
+        self.lstVisible.setModel(self.modeloVisible)
+        self.lstOculta.setModel(self.modeloOculto)
+
+
+        controlsInerte = QWidget()
         controlsInerte.setFixedSize(1,20)
 
         maia.addWidget(lblFollasVisibles)
         maia.addWidget(lblFollasOcultas,0,2,1,1)
-        maia.addWidget(lstVisible,1,0,5,1)
-        maia.addWidget(lstOculta,1,2,5,1)
+        maia.addWidget(self.lstVisible,1,0,5,1)
+        maia.addWidget(self.lstOculta,1,2,5,1)
         maia.addWidget(btnOcultar,1,1,1,1)
         maia.addWidget(btnMOstrar,3,1,1,1)
         maia.addWidget(btnMOstrar,7,1,1,1)
