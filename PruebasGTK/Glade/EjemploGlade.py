99517b12e3e7cfeaaf57fd38b95b4f34ee9e5419
jorge <jdurancruz@danielcastelao.org>
2025-11-20 09:09:04 +0100

    una cosa

--- PruebasGTK/Glade/EjemploGlade.py
+++ PruebasGTK/Glade/EjemploGlade.py
@@ -1,21 +1,41 @@
 import  gi
-from EjemploEssemtia.CaixaCor import CaixaCor
 gi.require_version("Gtk","3.0")
 from gi.repository import Gtk,Gdk,GObject
 
-"""Diseño
-
-rojo-marron-amarillo-naranja
-rojo-verde-verde-morado
-azul-azul-rosa-negro
-
-"""
 
 class EjemploGlade:
+    def on_cmbAlbara_changed(self,boton):
+        pass
+    def on_btnEngadir_clicked(self, boton):
+        pass
+    def on_btnEditar_clicked(self, boton):
+        pass
+    def on_btnBorrar_clicked(self, boton):
+        pass
+    def on_btnAceptar_clicked(self, boton):
+        pass
+    def on_btnCancelar_clicked(self, boton):
+        pass
+
+
     def __init__(self):
+
+        listaDetalleAlbara = [["00012",'']]
+
         builder = Gtk.Builder()
         builder.add_from_file("formularioAlbara.glade")
         wndPrincipal = builder.get_object("wndPrincipal")
+        self.trvDetallealbara = builder.get_object("trvDetallealbara")
+
+        sinais = {"on_wndPrincipal_delete_event":Gtk.main_quit,
+                  "on_cmbAlbara_changed":self.on_cmbAlbara_changed,
+                  "on_btnEngadir_clicked":self.on_btnEngadir_clicked,
+                  "on_btnEditar_clicked":self.on_btnEditar_clicked,
+                  "on_btnBorrar_clicked":self.on_btnBorrar_clicked,
+                  "on_btnAceptar_clicked":self.on_btnAceptar_clicked,
+                  "on_btnCancelar_clicked":self.on_btnCancelar_clicked}
+        builder.connect_signals(sinais)
+        self.trvDetallealbara.set_model()
 
 
 
