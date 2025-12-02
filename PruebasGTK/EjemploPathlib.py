d079f64e882b24daaaebcfea9111f7b2b70d18b1
jorge <jdurancruz@danielcastelao.org>
2025-12-02 13:48:02 +0100

    It Worksss!!!!!!!!

--- PruebasGTK/EjemploPathlib.py
+++ PruebasGTK/EjemploPathlib.py
@@ -10,9 +10,29 @@ class EjemploTree(Gtk.Window):
         for entrada in contidoDir.iterdir():
             if  entrada.is_dir():
                 punteiroFillo = modelo.append(punteiroPai, ("folder", entrada.name))
-                self.explorarDirectorio(ruta+'/'+entrada.name,punteiroFillo,modelo)
+                #self.explorarDirectorio(ruta+'/'+entrada.name,punteiroFillo,modelo)
             else:
                 modelo.append(punteiroPai,("emblem-documents", entrada.name))
+
+    def on_seleccion_changed(self, seleccion):
+        ruta = ""
+        modelo,fila = seleccion.get_selected()
+        if modelo[fila][0] == "folder":
+            ruta = self.obterRuta(modelo,fila)
+            self.explorarDirectorio("/home/dam/"+ruta,fila,modelo)
+
+
+
+    def obterRuta(self,modelo,fila):
+
+        punteiroPai = modelo.iter_parent(fila)
+
+        if punteiroPai is None:
+            return modelo[fila][1]
+        else:
+            return self.obterRuta(modelo,punteiroPai)+"/"+modelo[fila][1]
+
+
 
     def __init__(self):
         super().__init__()
@@ -22,6 +42,8 @@ class EjemploTree(Gtk.Window):
 
         modelo = Gtk.TreeStore(str,str)
         trvVista = Gtk.TreeView(model=modelo)
+        seleccion = trvVista.get_selection()
+        seleccion.connect("changed",self.on_seleccion_changed)
 
         tvcColumna = Gtk.TreeViewColumn()
         trvVista.append_column(tvcColumna)
@@ -35,7 +57,7 @@ class EjemploTree(Gtk.Window):
         tvcColumna.pack_start(celda, True)
         tvcColumna.add_attribute(celda, 'text', 1)
 
-        self.explorarDirectorio('/home/dam',None, modelo)
+        self.explorarDirectorio('/home/dam/',None, modelo)
 
         caixav.pack_start(trvVista,True,True,10)
 
