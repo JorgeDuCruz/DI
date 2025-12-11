a7fdcecd632db95fe4b5afaaf4ac8479986fae09
jorge <jdurancruz@danielcastelao.org>
2025-12-11 09:06:06 +0100

    Conectar señales de QTCreator

--- PruebaQT/probaQTCreator.py
+++ PruebaQT/probaQTCreator.py
@@ -9,11 +9,18 @@ class MinhaAplicacion:
         self.fiestra = loadUi("InterfaceQTCreator/form.ui")
         self.txtApelidos = self.fiestra.txtApelidos
         self.txtApelidos.setText("Hola")
+        self.cmbNumeroCliente = self.fiestra.cmbNumeroCliente
+        self.cmbNumeroCliente.insertItems(0,('1','2','3','4','5'))
+        self.btnEditar = self.fiestra.btnEditar
+        self.btnEditar.pressed.connect(self.on_btnEditar_pressed)
 
         self.fiestra.show()
 
     def run(self):
         sys.exit(self.aplicacion.exec())
+    def on_btnEditar_pressed(self):
+        numCliente = self.cmbNumeroCliente.currentText()
+        self.txtApelidos.setText("O numero de cliente é: "+numCliente)
 
 if __name__ == "__main__":
     apli = MinhaAplicacion()
