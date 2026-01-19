29858f67c62f0957d45cf32771c37c2e97b565d4
jorge <jdurancruz@danielcastelao.org>
2026-01-19 13:11:57 +0100

    Grafica de lineas horizonta

--- RepoLab/Charts.py
+++ RepoLab/Charts.py
@@ -1,9 +1,22 @@
 from reportlab.graphics.charts.barcharts import VerticalBarChart
+from reportlab.graphics.charts.linecharts import HorizontalLineChart
 from reportlab.graphics.shapes import Drawing
-from reportlab.platypus import  SimpleDocTemplate
+from reportlab.graphics.charts.textlabels import Label
+from reportlab.graphics.widgets.markers import makeMarker
+from reportlab.platypus import SimpleDocTemplate, Spacer
 from reportlab.lib.pagesizes import A4
 
 d = Drawing(400,200)
+titulo = Label()
+titulo.setOrigin(200,190)
+titulo.setText("Porcentaxe contratados/aprobados")
+d.add(titulo)
+
+lendaLateral = Label()
+lendaLateral.setOrigin(10,100)
+lendaLateral.angle = 90
+lendaLateral.setText("Porcentaxe")
+d.add(lendaLateral)
 
 datos = [(13.3,8,14.3,25,33.3,37.5,21.1,28.6,45.5,38.1,54.6,36.0,42.3),
          (67,69,68,81,92,90,87,82,77,79,59,69,61)]
@@ -29,5 +42,23 @@ graficoBarras.barSpacing = 5
 
 d.add(graficoBarras)
 
+d2 = Drawing(400,200)
+
+graficoLinhas = HorizontalLineChart()
+graficoLinhas.x = 30
+graficoLinhas.y = 50
+graficoLinhas.height = 125
+graficoLinhas.width = 350
+graficoLinhas.data = datos
+graficoLinhas.categoryAxis.categoryNames = lendaDatos
+graficoLinhas.categoryAxis.labels.boxAnchor = 'n'
+graficoLinhas.valueAxis.valueMin = 0
+graficoLinhas.valueAxis.valueMax = 100
+graficoLinhas.valueAxis.valueStep = 20
+graficoLinhas.lines[0].strokeWidth = 2
+graficoLinhas.lines[0].symbol = makeMarker('FilledCircle')
+graficoLinhas.lines[1].strokeWidth = 1.5
+d2.add(graficoLinhas)
+
 doc = SimpleDocTemplate("ExemploGrafico.pdf",pagesize=A4)
-doc.build([d])
+doc.build([d,Spacer(20,20),d2])
