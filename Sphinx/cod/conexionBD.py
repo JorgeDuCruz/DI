80a4554a222eaa75c908413f70acc0db1bc92fa9
jorge <jdurancruz@danielcastelao.org>
2026-02-05 09:34:46 +0100

    Params y returns bien comentados

--- Sphinx/cod/conexionBD.py
+++ Sphinx/cod/conexionBD.py
@@ -70,8 +70,8 @@ class ConexionBD:
     def consultaSenParametros (self, consultaSQL):
         """Retorna unha lista cos rexistros dunha consulta realizada sen pasarlle parámetros.
 
-        :param consultaSQL. Código da consulta sql a executar
-        :return listaConsulta
+        :param consultaSQL: Código da consulta sql a executar
+        :return: listaConsulta
 
         """
 
@@ -100,9 +100,9 @@ class ConexionBD:
 
         A consulta ten que estar definida con '?' na clausula where de SQL.
 
-        :param consultaSQL. Código da consulta sql a executar
-        :param *parametros. Parámetros para introducir na consulta
-        :return listaConsulta
+        :param consultaSQL: Código da consulta sql a executar
+        :param *parametros: Parámetros para introducir na consulta
+        :return: listaConsulta
 
         """
 
